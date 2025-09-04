2d015c98bac69b02f8b476ee239af53e0d9360f6
codedstrings <mridhulml@gmail.com>
2025-09-04 10:32:13 +0530

    feat: Refactor App component to implement basic drum machine functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+type DrumPad = {
+  key: number;
+  keyTrigger: string;
+  id: string;
+  url: string;
+};
+
+const bankOne: DrumPad[] = [
+  { key: 1, keyTrigger: 'Q', id: 'Heater-1', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+  { key: 2, keyTrigger: 'W', id: 'Heater-2', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+  { key: 3, keyTrigger: 'E', id: 'Heater-3', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+  { key: 4, keyTrigger: 'A', id: 'Heater-4', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+  { key: 5, keyTrigger: 'S', id: 'Clap', url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+  { key: 6, keyTrigger: 'D', id: 'Open-HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+  { key: 7, keyTrigger: 'Z', id: "Kick-n'-Hat", url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+  { key: 8, keyTrigger: 'X', id: 'Kick', url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+  { key: 9, keyTrigger: 'C', id: 'Closed-HH', url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [display, setDisplay] = useState('');
+
+  const onDrumPadClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const audio = e.currentTarget.querySelector('audio')!;
+    playAudio(audio);
+    setDisplay(e.currentTarget.id);
+  }
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    const pad = bankOne.find(p => p.keyTrigger === e.key.toUpperCase());
+    if (pad) {
+      const audio = document.getElementById(pad.keyTrigger) as HTMLAudioElement;
+      playAudio(audio);
+      setDisplay(pad.id);
+    }
+  }
+
+  const playAudio = (audioElement: HTMLAudioElement) => {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="drum-machine">
+        <div id="display">{display}</div>
+         {bankOne.map((pad) => (
+            <button key={pad.key} className="drum-pad" id={pad.id} onClick={onDrumPadClick} onKeyDown={onKeyDown}>
+              {pad.keyTrigger}
+              <audio className="clip" id={pad.keyTrigger} src={pad.url}></audio>
+            </button>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
